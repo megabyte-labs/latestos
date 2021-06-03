8ca3e5712102557d521388d9f3390904fcb515fd
Renny Montero <rennym19@gmail.com>
2021-06-03 14:11:42 -0400

    Support for Windows Insiders Preview

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ from setuptools import setup, find_packages
 setup(
     name='latestos',
     packages=find_packages(),
-    version='0.4.3',
+    version='0.5',
     license='MIT',
-    description='Latest OS version checker for Linux Distros using the Arizona Mirror',
+    description='Latest OS version checker for Linux Distros using the ' \
+                'Arizona Mirror and Windows 10 Insiders Preview',
     author='Renny Montero',
     author_email='rennym19@gmail.com',
     url='https://github.com/rennym19/latestos',
-    download_url='https://github.com/rennym19/latestos/archive/v_04_02.tar.gz',
-    keywords=['OS', 'LINUX', 'VERSION', 'CHECKER', 'SCRAPER'],
+    download_url='https://github.com/rennym19/latestos/archive/v_05.tar.gz',
+    keywords=['OS', 'LINUX', 'VERSION', 'CHECKER', 'SCRAPER', 'WINDOWS'],
     install_requires=[
         'requests',
         'lxml',
+        'selenium',
     ],
     include_package_data=True,
     entry_points={'console_scripts': ['latestos = latestos.commands:get_latest_os']},
